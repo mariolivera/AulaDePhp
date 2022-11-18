75b765ba7c49c5b530e35d58b1c529c49f777fe9
Mário Oliveira <113113372+mariolivera@users.noreply.github.com>
2022-11-18 12:36:35 -0300

    atualizando crud

--- php-poo-crud/src/controller/alunoController.php
+++ php-poo-crud/src/controller/alunoController.php
@@ -3,53 +3,69 @@
 //definindo que nesse arquivo será trabalhado os tipos de dados
 declare(strict_types=1);
 
-function soma(float $n1, float $n2): float
-{
-    return $n1 + $n2;
+function renderizar(string $nomeDoArquivo, mixed $dados = null) {
+    include '../src/views/head.phtml';
+    include "../src/views/{$nomeDoArquivo}.phtml";
+    $dados;
+    include '../src/views/foot.phtml';
 }
 
-function welcome(string $nome): string
-{
-    return "Bem vinde {$nome}";
+function redirecionar(string $onde){
+    header("location: {$onde}");
 }
 
-function concat(int $n1, int $n2): string
-{
-    return (string) $n1 . (string) $n2;
-}
 
 function inicio (): void //estamos declarando que essa funcao "nao tem retorno"
 {
-    include '../src/views/inicio.phtml';
+    renderizar("inicio");
+}
+
+function excluir() {
+    $id = $_GET['id']; //recuperando o id que tava na URL
+
+    excluirAluno($id); //pedindo ao repository pra excluir o aluno (nao sabemos como, mas ele vai)
+
+    redirecionar("/listar"); //redirecionando pra pagina de listar
 }
 
 function listar (): void 
 {
-    //SELECT TODOS
+    $alunos = buscarAlunos();
+    renderizar("listar", $alunos);
 
-    $alunos = abrirConexao()->query("SELECT * FROM tb_alunos");
-    include '../src/views/listar.phtml';
 }
 
 function novo (): void 
 {
-    //INSERT INTO
-    include '../src/views/novo.phtml';
-    novoAluno();
-    
+    renderizar("novo");
+
+    //se o usuario preencheu o formulario, vai entrar nesse if
+    if (false === empty($_POST)) {
+        $nome = trim($_POST['nome']);
+        $cidade = trim($_POST['cidade']);
+        $matricula = trim($_POST['matricula']);
+
+        if (true === validateForm($nome, $cidade, $matricula)) {
+            novoAluno($nome, $cidade, $matricula);
+            redirecionar('/listar');
+        } 
+    }
 }
 
 function editar (): void
 {
-    $id = $_GET ["id"];
+    $id = $_GET["id"];
     $aluno = buscarUmAluno($id);
-    atualizarAluno();
-    include '../src/views/editar.phtml';
-}
+    renderizar("editar", $aluno);
+    if (false === empty($_POST)) {
+        $nome = trim($_POST['nome']);
+        $cidade = trim($_POST['cidade']);
+        $matricula = trim($_POST['matricula']);
 
-function excluir ()
-{
-    $id = $_GET['id'];
-    excluirAluno($id); // pedindo ao repositorio para excluir o aluno 
-    header('location:/listar');
+        if (true === validateForm($nome, $cidade, $matricula)) {
+            atualizarAluno($nome, $cidade, $matricula, $id);
+            redirecionar('/listar');
+        } 
+    }
+    
 }
---
+++ php-poo-crud/src/repository/alunoRepository.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+function buscarAlunos(): iterable
+{
+    $sql = 'SELECT * FROM tb_alunos';
+
+    $alunos = abrirConexao()->query($sql);
+
+    return $alunos;
+}
+
+function buscarUmAluno($id): iterable
+{
+    $sql = "SELECT * FROM tb_alunos WHERE id ='{$id}'";
+
+    $aluno = abrirConexao()->query($sql);
+
+    return $aluno->fetch(PDO::FETCH_ASSOC);
+}
+
+function novoAluno(string $nome, string $cidade, string $matricula): void
+{
+    $sql = "INSERT INTO tb_alunos (nome, matricula, cidade) VALUES (?,?,?)";
+    $query = abrirConexao()->prepare($sql);
+    $query->execute([$nome, $matricula, $cidade]);
+}
+
+function atualizarAluno(string $nome, string $cidade, string $matricula, string $id): void
+{
+
+    $sql = "UPDATE tb_alunos SET nome=?, matricula=?, cidade=? WHERE id=?";
+    $query = abrirConexao()->prepare($sql);
+    $query->execute([$nome, $matricula, $cidade,$id]);
+}
+
+function excluirAluno(string $id): void
+{
+    $sql = "DELETE FROM tb_alunos WHERE id={$id}";
+
+    abrirConexao()->query($sql);
+}
+
